739b6206ee1a52bb1ee3cbbcc8bbed18f6c961b6
Raksha Kagadalu Raju <kagadaluraju.r@northeastern.edu>
2022-04-07 15:44:35 -0400

    renamed property search files

--- server/api/services/users-services.js
+++ server/api/services/users-services.js
@@ -1,25 +1,25 @@
 //Service layer - most of our business logic which supports the model - is present - ex- endpoint search-logic and checks to search is in service layer
 import User from "./../models/user.js";
 
-//create new todo item.
+//create new user
 export const save = (newUser) => {
   const user = new User(newUser);
   return user.save();
 };
 
-//returns all the todo items
+//returns all the users
 export const search = (query) => {
   const params = { ...query };
   return User.find(params).exec();
 };
 
-//search for todo item by id
+//search for user by id
 export const get = (id) => {
   const user = User.findById(id).exec();
   return user;
 };
 
-// Update an existing todo item by id
+// Update an existing user by id
 export const update = (updatedUser) => {
   updatedUser.lastModifiedDate = new Date();
   const user = User.findByIdAndUpdate(updatedUser.id, updatedUser, {
@@ -28,7 +28,7 @@ export const update = (updatedUser) => {
   return user;
 };
 
-//Delete an todo item by id
+//Delete an user by id
 export const remove = (id) => {
   const user = User.findByIdAndDelete(id).exec();
   return user;
