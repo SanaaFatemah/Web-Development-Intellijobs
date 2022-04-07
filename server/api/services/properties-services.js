0aae1b436b963c86c35e7c6d4cfc39b940cf3584
Raksha Kagadalu Raju <kagadaluraju.r@northeastern.edu>
2022-04-07 15:49:52 -0400

    updated property services

--- server/api/services/properties-services.js
+++ server/api/services/properties-services.js
@@ -1,35 +1,39 @@
 //Service layer - most of our business logic which supports the model - is present - ex- endpoint search-logic and checks to search is in service layer
-import House from "../models/property.js";
+import Property from "../models/property.js";
 
-//create new House
-export const save = (newUser) => {
-  const user = new User(newUser);
-  return user.save();
+//create new property
+export const save = (newProperty) => {
+  const property = new User(newProperty);
+  return property.save();
 };
 
 //returns all the users
 export const search = (query) => {
   const params = { ...query };
-  return User.find(params).exec();
+  return Property.find(params).exec();
 };
 
 //search for user by id
 export const get = (id) => {
-  const user = User.findById(id).exec();
-  return user;
+  const property = Property.findById(id).exec();
+  return property;
 };
 
 // Update an existing user by id
-export const update = (updatedUser) => {
-  updatedUser.lastModifiedDate = new Date();
-  const user = User.findByIdAndUpdate(updatedUser.id, updatedUser, {
-    new: true,
-  }).exec();
-  return user;
+export const update = (updatedProperty) => {
+  updatedProperty.lastModifiedDate = new Date();
+  const property = Property.findByIdAndUpdate(
+    updatedProperty.id,
+    updatedProperty,
+    {
+      new: true,
+    }
+  ).exec();
+  return property;
 };
 
 //Delete an user by id
 export const remove = (id) => {
-  const user = User.findByIdAndDelete(id).exec();
-  return user;
+  const property = Property.findByIdAndDelete(id).exec();
+  return property;
 };
