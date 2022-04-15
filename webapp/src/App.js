020797f9cebb91188d68c8aa585ecf3539c0dffb
Raksha Kagadalu Raju <kagadaluraju.r@northeastern.edu>
2022-04-15 08:25:17 -0400

    added sass

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
+import React from "react";
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <Router>
       <Home />
       <Routes>
-        <Route path='/' exact component={Home}/>
+        <Route path="/" exact component={Home} />
       </Routes>
     </Router>
   );
