9889574ab286b4b57145e53b54a675d9106e50e7
Raksha Kagadalu Raju <kagadaluraju.r@northeastern.edu>
2022-04-16 14:06:14 -0400

    Added Link to Error Page

--- webapp/src/pages/ErrorPage.js
+++ webapp/src/pages/ErrorPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <h1>ErrorPage</h1>;
+  return (
+    <div>
+      <h1>ErrorPage</h1>
+      <Link to="/">BackHome</Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
