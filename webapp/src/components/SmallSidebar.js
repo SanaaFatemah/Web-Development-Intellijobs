6b11a1408cdb620f51b80863c5796d5f7aa7b5b2
Ashish Kumar <ramesh.ashi@northeastern.edu>
2022-04-21 14:33:37 -0400

    changes for side bar in a small screen

--- webapp/src/components/SmallSidebar.js
+++ webapp/src/components/SmallSidebar.js
@@ -1,9 +1,25 @@
 import Wrapper from "../assets/wrappers/SmallSidebar"
+import {FaTimes} from 'react-icons/fa'
+import Logo from './Logo'
+import {links} from '../utilities/links'
+import {useContextApp} from '../context/contextApp'
+import {NavLink} from 'react-router-dom'
+
 const SmallSidebar = () => {
     return (
         <Wrapper>
             <h4>
-                Small Sidebar 
+                <div className="sidebar-container show-sidebar">
+                    <div className="content">
+                        <button type="button" className="close-btn" onClick={()=>{console.log('toggle sidebar')}}>
+                            <FaTimes />
+                        </button>
+                        <header>
+                            <Logo />
+                        </header>
+                        <div className="nav-links">nav links</div>
+                    </div>
+                </div>
             </h4>
         </Wrapper>
     )
