888f6f705c8437a7a3f96c5d838dffd6445e2394
Ashish Kumar <ramesh.ashi@northeastern.edu>
2022-04-24 19:14:02 -0400

    Area chart and bar chart using recharts

--- webapp/src/components/AreaChart.js
+++ webapp/src/components/AreaChart.js
@@ -1,7 +1,17 @@
-const AreaChart = () => {
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
+
+const AreaChartGraph = ({data}) => {
     return (
-        <h2>Area chart</h2>
+        <ResponsiveContainer width="100%" height={270}>
+        <AreaChart data={data} margin={{top:50}}>
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis dataKey='date'/>
+            <YAxis allowDecimals={false}/>
+            <Tooltip />
+            <Area type='monotone' dataKey='count' fill='#b2d7eb' stroke='#2cb1bc'/>
+        </AreaChart>
+        </ResponsiveContainer>
     )
 }
 
-export default AreaChart;
+export default AreaChartGraph;
--- webapp/src/components/BarChart.js
+++ webapp/src/components/BarChart.js
@@ -1,7 +1,16 @@
-const BarChart = () => {
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
+const BarChartGraph = ({data}) => {
     return (
-        <h2>Bar chart</h2>
+        <ResponsiveContainer width="100%" height={270}>
+        <BarChart data={data} margin={{top:50}}>
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis dataKey='date'/>
+            <YAxis allowDecimals={false}/>
+            <Tooltip />
+            <Bar dataKey='count' fill='#b2d7eb' barSize={75}/>
+        </BarChart>
+        </ResponsiveContainer>
     )
 }
 
-export default BarChart;
+export default BarChartGraph;
