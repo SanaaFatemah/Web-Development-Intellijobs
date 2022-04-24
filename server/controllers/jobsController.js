d64243012019cce490de9ef8632b83333a412524
Sanaa Fatemah Rupani <rupani.s@northeastern.edu>
2022-04-24 16:10:53 -0400

    Completed monthly applications

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -7,6 +7,7 @@ import {
 } from "../errors/index.js";
 import checkPermissions from "../utils/checkPermission.js";
 import mongoose from "mongoose";
+import moment from 'moment'
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -107,6 +108,19 @@ const showStats = async (req, res) => {
   {$sort:{'_id.year':-1, '_id.month':-1}},
   {$limit:6},
   ])
+
+  monthlyApplications = monthlyApplications.map((item) => {
+    const {
+      _id:{year,month},
+       count,
+      } = item
+      const date = moment()
+      .month(month-1)
+      .year(year)
+      .format('MMM Y')
+      return {date, count}
+  })
+  .reverse()
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
