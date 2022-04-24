19698b9231f68f85c05b15b308c024d9dec5f026
Sanaa Fatemah Rupani <rupani.s@northeastern.edu>
2022-04-24 14:28:31 -0400

    Stat cards

--- webapp/src/components/StatItem.js
+++ webapp/src/components/StatItem.js
@@ -1,10 +1,14 @@
-const StatItem = () => {
+import Wrapper from '../assets/wrappers/StatItem'
+
+const StatItem = ({count, title, icon, color, bcg}) => {
     return (
-        <div>
-            <h1>
-               single stats item
-            </h1>
-        </div>
+        <Wrapper color={color} bcg={bcg }>
+            <header>
+                <span className='count'>{count}</span>
+                <span className='icon'>{icon}</span>
+            </header>
+            <h5 className='title'>{title}</h5>
+        </Wrapper>
     )
     }
     
