632d6240af4416d05d2826b3a2528c7740948bfc
Raksha Kagadalu Raju <kagadaluraju.r@northeastern.edu>
2022-04-25 12:45:49 -0400

    added job limit in server for pagination

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -67,16 +67,19 @@ const getAllJobs = async (req, res) => {
 
   //using the limit and skip from mongoose to limit the number of jobs displayed
 
-  const limit = 10;
-  const skip = 1;
-
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
   result = result.skip(skip).limit(limit);
 
   const jobs = await result;
 
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  //to count the number of total jobs in all pages for a specific query search
+  const totalJobs = await Job.countDocuments(queryObject);
+  //to count the number of pages
+  const numOfPages = Math.ceil(totalJobs / limit); //ceil to round up
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages: numOfPages });
 };
 
 const updateJob = async (req, res) => {
